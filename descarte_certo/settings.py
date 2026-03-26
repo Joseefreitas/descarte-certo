7e7318db2532aebe8d05c75af51d644ea1e47285
Eduardo <jose.eduardo.b.freitas@gmail.com>
2026-03-26 05:30:06 -0300

    Configura PostgreSQL e atualiza dependências
    - Troca o banco de dados padrão (SQLite) pelo PostgreSQL no settings.py
    - Aplica as migrações no novo banco
    - Atualiza requirements.txt com a biblioteca psycopg2-binary
    - Adiciona pastas do VS Code (.vscode) ao .gitignore

--- descarte_certo/settings.py
+++ descarte_certo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'descarte_certo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'descarte_certo',
+        'USER': 'descarte_admin',
+        'PASSWORD': 'descarte_certo_2026', # Usem essa mesma senha quando criarem o banco de dados
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
