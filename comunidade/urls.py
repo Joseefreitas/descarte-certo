750ad7cd716478d78d615611676129e8dbd166b3
Eduardo <jose.eduardo.b.freitas@gmail.com>
2026-05-13 16:40:16 -0300

    fix: descomenta rota de deletar topic na comunidade

--- comunidade/urls.py
+++ comunidade/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.comunidade, name='comunidade'),
-    # para carregar foto 
     path('<int:topic_id>/', views.topic_detail, name='topic_detail'),
-    # para conseguir o usuário apagar uma postagem que ele fez
-    # path('<int:topic_id>/deletar/', views.deletar_topic, name='deletar_topic'),
+    path('<int:topic_id>/deletar/', views.deletar_topic, name='deletar_topic'),
 ]
