616bdd24dbe39c9057c0bfcb656f2322cbe87d4f
LeticiaCamelloBarros <lsncb@cesar.school>
2026-05-17 13:44:45 -0300

    ajeitando bug no cadastro de novo
    urls com o cadastro unico antigo,que já não faz mais sentido porque agora são dois tipos de cadastro

--- core/views.py
+++ core/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
 # ======================
-# HOME (protegida)
+# HOME
 # ======================
 def home(request):
     modo_visitante = request.session.get('modo_visitante', False)
-
     return render(request, 'index.html', {
         'modo_visitante': modo_visitante
     })
@@ -20,65 +18,12 @@ def home(request):
 def guia_descarte(request):
     return render(request, 'guia_descarte.html')
 
-
-# ======================
-# CADASTRO
-# ======================
-def cadastro(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
-
-        if User.objects.filter(username=username).exists():
-            return render(request, 'cadastro.html', {
-                'erro': 'Esse usuário já existe.'
-            })
-
-        user = User.objects.create_user(
-            username=username,
-            email=email,
-            password=senha
-        )
-
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save()
-
-        return redirect('login')
-
-    return render(request, 'cadastro.html')
-
-
-# ======================
-# LOGIN
-# ======================
-def login_usuario(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user:
-            login(request, user)
-            request.session.pop('modo_visitante', None)
-            return redirect('home')
-
-        return render(request, 'login.html', {'erro': 'Usuário ou senha inválidos.'})
-
-    return render(request, 'login.html')
-
-
 # ======================
 # MODO VISITANTE
 # ======================
 def modo_visitante(request):
     request.session['modo_visitante'] = True
     return redirect('home')
-
 
 # ======================
 # LOGOUT
--- descarte_certo/urls.py
+++ descarte_certo/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for descarte_certo project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include, re_path
 from core import views
@@ -22,16 +5,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
 from django.views.static import serve
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', views.login_usuario),
-    path('login/', views.login_usuario, name='login'),
-    path('cadastro/', views.cadastro, name='cadastro'),
+    path('', lambda request: redirect('/login/')), 
     path('logout/', views.logout_usuario, name='logout'),
     path('guest/', views.modo_visitante, name='guest'),
-
     path('home/', views.home, name='home'),
 
     path('comunidade/', include('comunidade.urls')),
@@ -44,4 +22,6 @@ urlpatterns = [
 
     path('coleta/', include('coleta.urls')),
     path('agendamento/', include('agendamento.urls')),
+
+    path('', include('login.urls')),  # login, cadastro, eusou aqui
 ]
