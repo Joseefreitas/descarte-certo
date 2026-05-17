e5149966349ff26c673b9fd041496ea25360365d
LeticiaCamelloBarros <lsncb@cesar.school>
2026-05-16 22:32:07 -0300

    ajeitando bug no cadastro de novo
    urls com o cadastro unico antigo,que já não faz mais sentido porque agora são dois tipos de cadastro

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,22 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.shortcuts import redirect
+from django.views.static import serve
 
 urlpatterns = [
-    path('', views.login_usuario, name='login'),
-    path('login/', views.login_usuario, name='login'),
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('home/', views.home, name='home'),
-    path('guia-descarte/', views.guia_descarte, name='guia_descarte'),
-    path('logout/', views.logout_usuario, name='logout'),
-    path('guest/', views.modo_visitante, name='guest')
+    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('/login/')),
+
+    path('', include('login.urls')),
+    path('', include('core.urls')),
+
+    path('comunidade/', include('comunidade.urls')),
+    path('mapa/', include('mapa.urls')),
+    path('reciclagem/', include('reciclagem.urls')),
+    path('coleta/', include('coleta.urls')),
+    path('agendamento/', include('agendamento.urls')),
+
+    path('accounts/login/', lambda request: redirect('/login/')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
