cbc74b720c722e2af5d8610f8773550e72414dd6
Eduardo <jose.eduardo.b.freitas@gmail.com>
2026-05-17 16:32:31 -0300

    fix: comenta rotas de cadastro empresa e pessoa física até views serem criadas

--- login/urls.py
+++ login/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('cadastro/', views.eusou, name='eusou'),
-    path('cadastro/empresa/', views.cadastro_empresa, name='cadastro_empresa'),
-    path('cadastro/pessoa-fisica/', views.cadastro_pessoa_fisica, name='cadastro_pessoa_fisica'),
+    #path('cadastro/empresa/', views.cadastro_empresa, name='cadastro_empresa'),
+    #path('cadastro/pessoa-fisica/', views.cadastro_pessoa_fisica, name='cadastro_pessoa_fisica'),
     path('login/', views.login_usuario, name='login'),
     path('logout/', views.logout_usuario, name='logout'),
 ]
