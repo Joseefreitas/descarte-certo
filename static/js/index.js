30e0a68d7f71e15421043cb780bb1f3b29fdd8fb
Dandara Resende <dmsrb@cesar.school>
2026-05-17 18:06:52 -0300

    arrumando home page

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,18 @@
-const tips = [
-    "Lave embalagens antes de reciclar para evitar contaminação.",
-    "Pilhas e eletrônicos devem ser levados a pontos específicos de coleta.",
-    "Separar o lixo orgânico ajuda na compostagem e reduz resíduos.",
-    "Caixas de papelão devem estar secas antes da reciclagem.",
-    "Vidros quebrados devem ser embalados com cuidado antes do descarte."
-];
+const slides = document.querySelectorAll('.slide');
 
-function setRandomTip() {
-    const el = document.getElementById("tip-text");
+let currentSlide = 0;
 
-    if (!el) return;
+function nextSlide() {
 
-    const random = Math.floor(Math.random() * tips.length);
-    el.textContent = tips[random];
+    slides[currentSlide].classList.remove('active');
+
+    currentSlide++;
+
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+
+    slides[currentSlide].classList.add('active');
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    setRandomTip();
-
-    setInterval(setRandomTip, 10000); // troca a cada 10s
-});
+setInterval(nextSlide, 4000);
