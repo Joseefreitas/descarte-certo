d17e6121a49372622a66bc78c3166dced8afaa2f
Eduardo <jose.eduardo.b.freitas@gmail.com>
2026-05-18 07:38:06 -0300

    fix: adiciona logging para produção

--- descarte_certo/settings.py
+++ descarte_certo/settings.py
@@ -172,3 +172,24 @@ SESSION_COOKIE_SECURE = not DEBUG
 CSRF_COOKIE_SECURE = not DEBUG
 SESSION_COOKIE_SAMESITE = 'Lax'
 CSRF_COOKIE_SAMESITE = 'Lax'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
